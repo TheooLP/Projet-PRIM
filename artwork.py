d9fe9f260b7531a47ce17f26c9e520138566eb52
TheooLP <82284037+TheooLP@users.noreply.github.com>
2023-12-05 22:05:41 +0100

    Update artwork.py : working

--- artwork.py
+++ artwork.py
@@ -4,28 +4,18 @@ import smartpy as sp
 
 @sp.module
 def main():
-    class Artist(sp.Contract):
-        def __init__(self, artist_address):
-            self.data.artist = artist_address
-            self.data.artwork_contracts = sp.set()
-
-        @sp.entry_point
-        def generate_certificate(self, properties):
-            
-
-    
     class Artwork(sp.Contract):
-        def __init__(self, properties):
-            self.id = sp.self_address
-            self.data.artist = sp.sender
-            self.data.owner = sp.sender
+        def __init__(self, params):
+            self.data.id = sp.nat(0)
+            self.data.artist = params.artist
+            self.data.owner = params.artist
             self.data.sold = False
-            self.data.properties = properties
+            self.data.properties = params.properties
 
         @sp.entry_point
         def modify_artwork(self, properties):
-            self.check_sender_is_owner_or_artist(artwork)
-            artwork.properties = properties
+            self.check_sender_is_owner_or_artist()
+            self.data.properties = properties
 
         @sp.entry_point
         def transfer_artwork(self, params):
@@ -55,6 +45,26 @@ def main():
         @sp.private
         def check_positive_price(self, price):
             assert price > 0, "Price must be positive"
+
+    class Artist(sp.Contract):
+        def __init__(self, artist_address):
+            self.data.artist = artist_address
+            self.data.artwork_contracts = sp.set()
+
+        # Params :
+        #    - id
+        #    - properties
+        @sp.entry_point
+        def generate_certificate(self, id, properties):
+            params = sp.record(
+                id = id,
+                artist = self.data.artist,
+                owner = self.data.artist,
+                sold = False,
+                properties = properties
+            )
+            a = sp.create_contract(Artwork, None, sp.mutez(0), params)
+            self.data.artwork_contracts.add(a)
             
 @sp.add_test(name="Artwork Test")
 def test():
@@ -63,10 +73,10 @@ def test():
 
     artist_address = sp.address("tz1ArtistAddress1234567890")
     buyer_address = sp.address("tz1BuyerAddress1234567890")
-    contract = main.MyContract(artist_address)
+    contract = main.Artist(artist_address)
     scenario += contract
 
-    properties = sp.record(
+    properties0 = sp.record(
         title="Mona Lisa",
         url="https://image.url",
         creation_date=sp.now,
@@ -78,7 +88,17 @@ def test():
         description="A masterpiece",
         edition_number=1
     )
-    contract.generate_certificate(id=1, properties=properties).run(sender=artist_address)
-    contract.transfer_artwork(id=1, new_owner=buyer_address, purchase_price=1100).run(sender=artist_address)
-    contract.update_sale_price(id=1, sale_price=2000).run(sender=artist_address, valid=False)
-    contract.update_sale_price(id=1, sale_price=1400).run(sender=buyer_address)
+    contract.generate_certificate(id=0, properties=properties0).run(sender=artist_address)
+    properties1 = sp.record(
+        title="La Cene",
+        url="https://image.url",
+        creation_date=sp.now,
+        type="Painting",
+        styles=["Art chretien", "Peinture d'histoire"],
+        sale_price=300,
+        last_purchase_price=0,
+        dimensions="460×880 cm",
+        description="Refectoire de couvent",
+        edition_number=2
+    )
+    contract.generate_certificate(id=0, properties=properties1).run(sender=artist_address)
